cb397a4a5a2d45cb333c92f1a459afd02fc5f7d8
birtanem <64389378+birtanem@users.noreply.github.com>
2020-12-01 16:11:22 +0900

    Update GoogleAuthentication.java

--- booksalon/src/mailtest/GoogleAuthentication.java
+++ booksalon/src/mailtest/GoogleAuthentication.java
@@ -6,7 +6,7 @@ public class GoogleAuthentication extends Authenticator {
 	PasswordAuthentication passAuth;
     
     public GoogleAuthentication(){
-        passAuth = new PasswordAuthentication("eneppy", "vvcqysjqtlasznhi");
+        passAuth = new PasswordAuthentication("id", "pass");
     }
  
     public PasswordAuthentication getPasswordAuthentication() {
